d996005ab622e211c970362ebb5636f80bb653a0
Clement Foo <e0726745@u.nus.edu>
2023-02-06 20:39:34 +0800

    Complete problem7
    Write query

---
+++ src/problem7/query.sql
@@ -0,0 +1,23 @@
+WITH usd_amount AS (
+    SELECT 
+        address, 
+        SUM(
+            CASE 
+                WHEN denom = 'usdc' THEN amount * 0.000001
+                WHEN denom = 'swth' THEN amount * 0.00000005
+                WHEN denom = 'tmz' THEN amount * 0.003
+            END
+        ) AS total_balance
+        FROM 
+            balances
+        GROUP 
+            BY address
+)
+SELECT 
+    *
+FROM 
+    usd_amount
+JOIN trades ON usd_amount.address = trades.address
+WHERE 
+    total_balance >= 500 
+    AND trades.block_height > 730000
